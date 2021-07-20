eb414f0eb8a28f150efd5c369ffcff039376b4aa
Your Name <09taumang@gmail.com>
2021-07-21 00:20:17 +0200

    a new start.

--- src/components/player_ui.js
+++ src/components/player_ui.js
@@ -3,37 +3,7 @@ import Image from '../images/street_lights.jpg';
 
 export default function player_ui() {
     return (
-        <div className="bg-indigo-900">
-            asdasd
-            <div className="bg-gray-500">
-                asdasd
-                {/* Artist Album art */}
-                <img src={Image} alt="street_lights" />
-            {/* artist name and song */}
-            <div>
-                <div>
-                <p>Closer</p>
-                </div>
-                <div>
-                Azure
-                </div>
-            </div>
-            
-            <div 
-            className="line -ml-4 relative w-24 h-2 rounded-lg">
-
-            </div>
-
-            <div>
-                <button
-                className="btn-Play_Pause">Play</button>
-                <button
-                 className="btn-Next">Next</button>
-                <button
-                 className="btn-Previous">Previous</button>
-            </div>
-            </div>
-
+        <div className="">
         </div>
     )
 }
