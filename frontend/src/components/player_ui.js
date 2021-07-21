9a93d1d653ed8502e70b74e367e48de89c5b3932
Your Name <09taumang@gmail.com>
2021-07-21 12:08:38 +0200

    new way.

--- frontend/src/components/player_ui.js
+++ frontend/src/components/player_ui.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Image from '../images/street_lights.jpg';
+
 
 export default function player_ui() {
     return (
-        <div className="">
+        <div>
+            
         </div>
     )
 }
